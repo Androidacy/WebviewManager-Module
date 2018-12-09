a0f4693197ceb1442eea99d1ad90ef767be0f84d
Zackptg5 <zackptg5@comcast.net>
2018-12-08 19:27:49 -0500

    1.8.1

--- common/sepolicy.sh
+++ common/sepolicy.sh
@@ -1,6 +1,2 @@
-# Do not concatenate classes 
-# Only concatenate permissions (the last argument in the statement)
-# NOT valid example: audioserver { audioserver_tmpfs mediaserver_tmpfs } file read
-#
-# Valid entry example: audioserver audioserver_tmpfs file read
-# Another valid example: audioserver audioserver_tmpfs file read,write,execute
+# Put sepolicy statements here
+# Example: allow { audioserver mediaserver } audioserver_tmpfs file { read write open }
