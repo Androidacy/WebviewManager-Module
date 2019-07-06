2965629f8f1084c9069a5ea45a5ddab293434f2b
alexa-v2 <bamnell@outlook.com>
2019-07-06 19:27:16 -0400

    bugfixes add more bugs
    bugfixes add more bugs

--- common/service.sh
+++ common/service.sh
@@ -1,13 +1,6 @@
-#!/system/bin/sh
-# Do NOT assume where your module will be located.
-# ALWAYS use $MODDIR if you need to know where this script
-# and module is placed.
-# This will make sure your module will still work
-# if Magisk change its mount point in the future
 MODDIR=${0%/*}	
-# This script will be executed in late_start service mode
 # wait until boot completed
-# until [ `getprop sys.boot_completed`. = 1. ]; do sleep 1; done
+until [ `getprop sys.boot_completed`. = 1. ]; do sleep 1; done
 # Bromite WebView needs to be installed as user app to prevent crashes
-pm install -r $MODDIR/system/app/*/*.apk
+pm install -r $MODDIR/system/app/webview/webview.apk
 rm -f $MODDIR/service.sh
