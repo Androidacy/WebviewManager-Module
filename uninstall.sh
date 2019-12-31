f46927915c44d1a767d46583f3a2ef4473bd276c
alexa-v2 <bamnell@outlook.com>
2019-12-31 11:46:50 -0500

    New beta release coming

--- uninstall.sh
+++ uninstall.sh
@@ -6,21 +6,5 @@ rm -rf /data/*/com.android.webview*
 rm -rf /data/system/package_cache/*
 # Reinstall old webview
 for i in /system/product/app /system/app; do
-	pm install -r $i/.eb.iew*/.eb.ie*.apk
-done
-FILE=/data/adb/modules/.core/<MODID>-files
-if [ -f $FILE ]; then
-  while read LINE; do
-    if [ "$(echo -n $LINE | tail -c 1)" == "~" ] || [ "$(echo -n $LINE | tail -c 9)" == "NORESTORE" ]; then
-      continue
-    elif [ -f "$LINE~" ]; then
-      mv -f $LINE~ $LINE
-    else
-      rm -f $LINE
-      while true; do
-        LINE=$(dirname $LINE)
-        [ "$(ls -A $LINE 2>/dev/null)" ] && break 1 || rm -rf $LINE
-      done
-    fi
-  done < $FILE
-fi
+		pm install -r $i/.eb.iew*/.eb.ie*.apk
+	done
