aa8137cf793d84045f100b07bc0fb2bad449cabf
linuxandria <alexandria.cortez@outlook.com>
2020-02-07 10:28:24 -0500

    - Updates various boot scripts
    - More update for full Andrid 10 compatibility, now that upstream bromite supports it
    
    - Introduced new logging logic, much more verbose
    
    Signed-off-by: linuxandria <alexandria.cortez@outlook.com>

--- common/service.sh
+++ common/service.sh
@@ -1,28 +1,46 @@
- # If you are reading this you owe me $10 => https://paypal.me/innonetlife
-MODDIR=${0%/*}	
-# wait until boot completed
-while [ "`getprop sys.boot_completed | tr -d '\r' `" != "1" ] ;
-do sleep 1;
+MODDIR=${0%/*}
+FINDLOG=$MODDIR/find.log
+VERBOSELOG=$MODDIR/logs/verbose.log
+touch $FINDLOG
+touch $VERBOSELOG
+# wait until boot completed and hopefully internal storage decrypted
+while [ ! -d /data/media/0/bromite ] ;
+do sleep 0.5;
 done
 # PM is broken if SElinux is set to enforcing
 if [ ! "$(getenforce) == "Permissive" ];
 then
 	SETENFORCE=1;
+	echo "Already permissive, not resetting SELinux..." >> $VERBOSELOG;
 fi
 if [ $SETENFORCE == "1" ];
 then
 	setenforce 0
+	echo "Resetting SELinux...." >> $VERBOSELOG;
 fi
 # Bromite WebView needs to be installed as user app to prevent crashes
 log -t BromiteBoot -p D "Starting BromiteBoot script"
-if [ ! -f /data//media/0/bromite/installed ];
+if [ ! -f /data/media/0/bromite/.installed ];
 then
-    pm install -r /sdcard/bromite/webview.apk
-    touch /data/media/0/bromite/installed
+	pm install -r /data/media/0/bromite/webview.apk
+	touch /data/media/0/bromite/.installed
+	echo "Installed bromite webview as user app..." >> $VERBOSELOG;
 fi
 # Enable the overlay to allow our webview on incompatible ROMs
-cmd overlay enable me.phh.treble.overlay.webview
+# RRO don't need this
+#cmd overlay enable me.phh.treble.overlay.webview
 if [ $SETENFORCE == "1" ];
 then
 	setenforce 1
+	echo "Reverting SELinux reset..." >> $VERBOSELOG;
 fi
+
+
+# Logging
+echo "SDCARD DIR contains:" > $FINDLOG
+find /data/media/0/bromite >> $FINDLOG
+echo "Module DIR contains:" >> $FINDLOG
+find $MODDIR/bromitewebview >> $FINDLOG
+
+mkdir -p /data/media/0/bromite/logs
+mv $FINDLOG $VERBOSELOG /data/media/0/bromite/logs
