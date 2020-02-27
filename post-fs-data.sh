4c96706fc9b608e52d25191e566d6a23d8d616b9
linuxandria <alexandria.cortez@outlook.com>
2020-02-27 15:03:37 -0500

    Fixes for boot scripts. Prevents a rare abort
    Fix file not being removed on install
    
    Signed-off-by: linuxandria <alexandria.cortez@outlook.com>

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,4 +1,9 @@
-# Set some variables
+#!/system/bin/sh
+MODDIR=${0%/*}
+INFO=/data/adb/modules/.bromitewebview-files
+MODID=bromitewebview
+LIBDIR=/system
+MODPATH=/data/adb/modules/bromitewebview
 SH=$(readlink -f "$0")
 MODDIR=$(dirname "$SH")
 exxit() {
@@ -6,65 +11,53 @@ exxit() {
 	    [ $1 -ne 0 ] && abort "$2"
 	      exit $1
       }
-
 mkdir -p $MODDIR/logs
 exec 2>$MODDIR/logs/postfsdata-verbose.log
 set -x
 set -euo pipefail
 trap 'exxit $?' EXIT
-# Determine where we're running from
-# Set up logging. Much info, much wow
 FINDLOG=$MODDIR/logs/find.log
 PROPSLOG=$MODDIR/logs/props.log
 mkdir -p $MODDIR/logs
 touch $FINDLOG
-# Verbose logs ON
-OL="me.phh.treble.overlay.webview"
+OL="com.linuxandria.android.webviewoverlay"
 LIST="/data/system/overlays.xml"
 DR="$(find /system /system/product /vendor -maxdepth 1 | grep overlay)"
 API="$(getprop ro.build.version.sdk)"
-
-# Logging stuffs
 touch $PROPSLOG
 echo "Firing up logging NOW "
-echo "---------- Device info: -----------" >> $PROPSLOG
+echo "---------- Device info: -----------" > $PROPSLOG
 getprop >> $PROPSLOG
 echo "------- End Device info ----------" >> $PROPSLOG
-
-# Determines if we've already foricbly enabled our overlay
-if [ grep '$OL' $LIST ] ;
+if grep 'com.linuxandria.android.webviewoverlay' /data/system/overlays.xml ;
 then
 	echo "Overlay already enabled, exiting"
-	export CT=1;
+	set CT="1" ;
 fi
-# Try to determine if the running ROM is custom or stock. Why can't custom ROMs just say they're custom? Sheesh
-# Also Android 10 shouldn't need the webview, needs more testing
-CUSTOM=$(getprop | grep 'havoc\|resurrection\|userdebug\|test-keys\|lineage\|dev-keys\|maintainer')
-if typeset -p custom 2> /dev/null | grep -q '^'; then
+if [ "getprop | grep 'havoc\|resurrection\|userdebug\|test-keys\|lineage\|dev-keys\|maintainer'" ];
+then
 	echo "Custom ROM is running"
-	CT=1;
+	set CT="1" ;
 fi
 if [ "$API" == "29" ];
 then
 	echo "Android 10 detected"
-	CT=1;
+	CT="1" ;
 fi
-if [ "$API" <= "27" ];
+if [ "$API" -lt "27" ];
 then
-	MODE=3;
+	set MODE="3" ;
 else
-	mode=6;
+	set MODE="6" ;
 fi
-# If we are assuming this is a stock ROM, then we need to force it to recognize our overlay
-# Not actually sure this is needed. Android may take care of this for us
 if  [ ! "$CT" == "1" ];
 then
  echo "Forcing the system to register our overlay..."
  sed -i "s|</overlays>|    <item packageName=\"${OL}\" userId=\"0\" targetPackageName=\"android\" baseCodePath=\"${DR}/WebviewOverlay.apk\" state=\"${MODE}\" isEnabled=\"true\" isStatic=\"true\" priority=\"98\" /></overlays>|" $LIST
 fi
-# If we are assuming this is a custom ROM, send our overlay into the void because most don't enforce Google webviews
 if [ "$CT" == "1" ];
 then
 	echo "Sending out overlay into the void..."
 	rm -rf $MODDIR/system/product $MODDIR/system/vendor $MODDIR/system/overlay;
 fi
+
