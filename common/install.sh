fe2b807fd1a0baefc95af982fae1d1e088390355
Alexandria C <alexandria.cortez@linuxandria.com>
2020-08-18 11:06:17 -0400

    Bugfixes and performance improvements
    - Refactor cdoe so that less code has to run, speeding up execution time
    
    - Verify download for security
    
    - Actually check if internet access is available instead of using a dirty hack
    
    - Resolve a few crashes
    
    - Replace google webivew if present
    
    Signed-off-by: Alexandria C <alexandria.cortez@linuxandria.com>

--- common/install.sh
+++ common/install.sh
@@ -1,132 +1,180 @@
 # Here we set up the internal storage location
 $BOOTMODE && SDCARD=/storage/emulated/0 || SDCARD=/sdcard
-mkdir $MODPATH/logs
+mkdir "$MODPATH"/logs
 VERSIONFILE='/sdcard/bromite/version'
-alias aapt='$MODPATH/common/tools/aapt'
-alias sign='$MODPATH/common/tools/zipsigner'
-alias curl='$MODPATH/common/tools/curl-$ARCH32'
-chmod -R 0755 $MODPATH/common/tools
+alias aapt='"$MODPATH"/common/tools/aapt'
+alias sign='"$MODPATH"/common/tools/zipsigner'
+alias curl='"$MODPATH"/common/tools/curl-${ARCH}'
+chmod -R 0755 "$MODPATH"/common/tools
 # Thanks SKittles9832 for the code I shamelessly copied :)
 VEN=/system/vendor
 [ -L /system/vendor ] && VEN=/vendor
 if [ -f $VEN/build.prop ]; then BUILDS="/system/build.prop $VEN/build.prop"; else BUILDS="/system/build.prop"; fi
 # MIUI screws with our overlay and no one wants to tell me how to fix it
 MIUI=$(grep "ro.miui.ui.version.*" $BUILDS)
-if [ $MIUI ]; then
+if [ "$MIUI" ]; then
   ui_print " MIUI is not supported, unless someone tells me how"
   abort " Aborting..."
 fi
-if  test $API -eq 30 ;
+if  test "$API" -eq 30 ;
 then
 	ui_print "Android 11 is not supported!"
-	ui_print "Aborting..."
-	abort ;
+	abort "Aborting...";
 fi
 ui_print "- $ARCH SDK $API system detected, selecting the appropriate files"
 # Set up version check
+mkdir -p /sdcard/bromite
 if [ ! -f /sdcard/bromite/version ];
 then
 	mktouch $VERSIONFILE
 	echo "0" > $VERSIONFILE;
 fi
-if [ "$BOOTMODE" = true ];
-then
-	mkdir -p /sdcard/bromite
+# Handle version upgrades
+rm -rf /sdcard/bromite/webview.apk
+download_webview () {
 	ui_print "- Downloading extra files please be patient..."
 	V=$(curl -k -L --silent "https://api.github.com/repos/bromite/bromite/releases/latest" |   grep '"tag_name":' |  sed -E 's/.*"([^"]+)".*/\1/')
-	URL=https://github.com/bromite/bromite/releases/download/${V}/${ARCH}_SystemWebView.apk
-	if [ -f /sdcard/bromite/webview.apk ] ;
+	URL="https://github.com/bromite/bromite/releases/download/${V}/${ARCH}_SystemWebView.apk"
+	if [ -f /sdcard/bromite/"${ARCH}"_SystemWebView.apk ] ;
 	then
-		if [ "$(cat $VERSIONFILE|tr -d '.')" -lt "$(echo ${V}|tr -d '.')" ];
+	# Only re-download if it's an upgrade
+		if [ "$(tr -d '.' < "$VERSIONFILE")" -lt "$(echo "${V}"|tr -d '.')" ];
 		then
-			curl -k -L -o /sdcard/bromite/webview.apk $URL
-			echo "${V}" > $VERSIONFILE
-		fi;
+			curl -k -L -o /sdcard/bromite/"${ARCH}"_SystemWebView.apk "$URL"
+			echo "${V}" > "$VERSIONFILE"
+		fi
 	else
-	# If the file doesn't exist, let's attempt a download anyway
-		curl -k -L -o /sdcard/bromite/webview.apk $URL
-		echo "${V}" > $VERSIONFILE;
+		# If the file doesn't exist, let's attempt a download anyway
+		curl -k -L -o /sdcard/bromite/ "$URL"
+		echo "${V}" > "$VERSIONFILE";
 	fi
-	ui_print "- Extracting webview files..."
-	if [ ! -f /sdcard/bromite/webview.apk ];
-	then
-		# File wasn't found and all attempts to download failed
-		ui_print "Sorry! A problem occurred."
-		ui_print "No capable apk was found, the files failed to download, or both!"
-		ui_print "Check your internet and try again"
-		abort;
-	fi
-   	paths=$(cmd package dump com.android.webview | grep codePath)
-	APKPATH=$(echo ${paths##*=})
-	cp_ch ${SDCARD}/bromite/webview.apk $MODPATH$APKPATH/webview.apk
-	touch $MODPATH$APKPATH/.replace
-	cp $MODPATH$APKPATH/webview.apk /tmp/webview.zip 
-	mkdir /tmp/webview -p
-	unzip -d /tmp/webview /tmp/webview.apk
-	cp -rf /tmp/webview/lib $MODPATH$APKPATH/
-	rm -rf /tmp/webview /tmp/webview.apk
-# If we're runnning under TWRP, try to copy the apk, else we need to download it so abort
-# Unnecessary. mmt-ex doesn't allow TWRP installs. Probably should remove this but it breaks stuff so it stays...
-elif [ "$BOOTMODE" = false ];
+}
+verify_webview () {
+	ui_print "Verifying files..."
+	curl -L -K -o "$TMPDIR"/brm_"${V}".sha256.txt "https://github.com/bromite/bromite/releases/download/${V}/brm_$(V).sha256.txt"
+	cd /sdcard/bromite/ && sha256sum -s --check "$TMPDIR"/brm_"${V}".sha256.txt && cd -
+	while $? != 0 ;
+	do
+		ui_print "Verification failed, retrying download"
+		rm -f /sdcard/bromite/"${ARCH}"_SystemWebView.apk
+		download_webview
+		verify_webview ;
+	done
+	ui_print "Verified successfully. Proceeding..."
+}
+ping -c 1 -q github.com >&/dev/null
+if test $? -eq 0 ;
 then
-	ui_print "Sorry! TWRP installs are not supported at this time!"
+	download_webview
+	verify_webview ;
+elif test ! -f /sdcard/bromite/"${ARCH}"_SystemWebView.apk ;
+then
+	# File wasn't found and all attempts to download failed
+	ui_print "Sorry! A problem occurred."
+	ui_print "No capable apk was found, the files failed to download, or both!"
+	ui_print "Check your internet and try again"
 	abort ;
 fi
+# Try to determine existing webview install
+unset APKPATH
+paths=$(cmd package dump com.android.webview | grep codePath)
+APKPATH=$(${paths##*=})
+if test -z ${var+APKPATH} ;
+then
+	paths=$(cmd package dump com.google.android.webview | grep codePath)
+	APKPATH=$(${paths##*=});
+fi
+if test -z ${var+APKPATH} ;
+then
+	APKPATH="system/app/webview";
+fi
+cp_ch ${SDCARD}/bromite/"${ARCH}"_SystemWebView.apk "$MODPATH"$APKPATH/webview.apk
+touch "$MODPATH"$APKPATH/.replace
+cp "$MODPATH"$APKPATH/webview.apk "$TMPDIR"/webview.zip 
+mkdir "$TMPDIR"/webview -p
+unzip -d "$TMPDIR"/webview /"$TMPDIR"/webview.apk
+cp -rf "$TMPDIR"/webview/lib "$MODPATH"$APKPATH/
+rm -rf "$TMPDIR"/webview "$TMPDIR"/webview.apk
 ui_print "!!!!!!!!!!!!!!! VERY IMPORTANT PLEASE READ!!!!!!!!!!!!!!!!!"
 ui_print "Reboot immediately after flashing or you may experience some issues! "
-ui_print "Also, if you had any other webview such as Google webview, you may want to re-enable it but beware conflicts"
+ui_print "Also, if you had any other webview such as Google webview, you may want to re-enable it"
+UI_PRINT "but beware conflicts"
 ui_print "Next boot may take significantly longer, we have to clear Dalvik cache here"
-if [ "${API}" == "29" ];
+if test"${API}" == "29" ;
 then
     ui_print "Android 10 detected"
-		aapt p -f -v -M ${MODPATH}/common/overlay10/AndroidManifest.xml \
-                -I /system/framework/framework-res.apk -S ${MODPATH}/common/overlay10/res \
-                -F ${MODPATH}/unsigned.apk &>$MODPATH/logs/aapt.log
+		aapt p -f -v -M "$MODPATH"/common/overlay10/AndroidManifest.xml \
+                -I /system/framework/framework-res.apk -S "$MODPATH"/common/overlay10/res \
+                -F "$MODPATH"/unsigned.apk &>"$MODPATH"/logs/aapt.log
 else
 	ui_print "Android version less than 10 detected"
-	aapt p -f -v -M ${MODPATH}/common/overlay9/AndroidManifest.xml \
-							-I /system/framework/framework-res.apk -S ${MODPATH}/common/overlay9/res \
-							-F ${MODPATH}/unsigned.apk &>$MODPATH/logs/aapt.log
+	aapt p -f -v -M "$MODPATH"/common/overlay9/AndroidManifest.xml \
+							-I /system/framework/framework-res.apk -S "$MODPATH"/common/overlay9/res \
+							-F "$MODPATH"/unsigned.apk &>"$MODPATH"/logs/aapt.log
 fi
-if [ -s ${MODPATH}/unsigned.apk ]; then
-	sign ${MODPATH}/unsigned.apk ${MODPATH}/signed.apk
-	cp -rf ${MODPATH}/signed.apk ${MODPATH}/common/WebviewOverlay.apk
-	rm -rf ${MODPATH}/signed.apk ${MODPATH}/unsigned.apk
+if [ -s "$MODPATH"/unsigned.apk ]; then
+	sign "$MODPATH"/unsigned.apk "$MODPATH"/signed.apk
+	cp -rf "$MODPATH"/signed.apk "$MODPATH"/common/WebviewOverlay.apk
+	rm -rf "$MODPATH"/signed.apk "$MODPATH"/unsigned.apk
 else
 	ui_print "Overlay creation has failed! Some ROMs have this issue"
 	ui_print "Compatibility cannot be gauraunteed, contact me on telegram to try to fix!"
 fi
 if [ -d /product/overlay ];
 then
-      mkdir -p $MODPATH/system/product/overlay
-			cp_ch $MODPATH/common/WebviewOverlay.apk $MODPATH/system/product/overlay;
-			echo "/product/oeverlay" > $MODPATH/location;
+      mkdir -p "$MODPATH"/system/product/overlay
+			cp_ch "$MODPATH"/common/WebviewOverlay.apk "$MODPATH"/system/product/overlay;
+			echo "/product/oeverlay" > "$MODPATH"/overlay;
 elif [ -d /vendor/overlay ]
 then
-	mkdir -p $MODPATH/system/vendor/overlay
-	cp_ch $MODPATH/common/WebviewOverlay.apk $MODPATH/system/vendor/overlay;
-	echo "/vendor/oeverlay" > $MODPATH/location;
+	mkdir -p "$MODPATH"/system/vendor/overlay
+	cp_ch "$MODPATH"/common/WebviewOverlay.apk "$MODPATH"/system/vendor/overlay;
+	echo "/vendor/oeverlay" > "$MODPATH"/overlay;
 elif [ -d /system/overlay ]
 then
-	mkdir -p $MODPATH/system/overlay
-	cp_ch $MODPATH/common/WebviewOverlay.apk $MODPATH/system/overlay;
-	echo "/system/oeverlay" > $MODPATH/location;
+	mkdir -p "$MODPATH"/system/overlay
+	cp_ch "$MODPATH"/common/WebviewOverlay.apk "$MODPATH"/system/overlay;
+	echo "/system/oeverlay" > "$MODPATH"/overlay;
 fi
 ui_print "- Cleaning up..."
-mkdir -p $MODPATH/apk
-cp_ch /sdcard/bromite/webview.apk $MODPATH/apk
-rm -f $MODPATH/system/app/placeholder
+mkdir -p "$MODPATH"/apk
+cp_ch /sdcard/bromite/webview.apk "$MODPATH"/apk
+rm -f "$MODPATH"/system/app/placeholder
 mkdir -p /sdcard/bromite/logs
-rm -f $MODPATH/*.md
+rm -f "$MODPATH"/*.md
 ui_print "- Backing up important stuffs"
-mkdir -p $MODPATH/backup/
-cp /data/system/overlays.xml $MODPATH/backup/
+mkdir -p "$MODPATH"/backup/
+cp /data/system/overlays.xml "$MODPATH"/backup/
 ui_print " "
 ui_print " "
+ui_print " ______   _______  _______  _______ __________________ _______                   
+(  ___ \ (  ____ )(  ___  )(       )\__   __/\__   __/(  ____ \                  
+| (   ) )| (    )|| (   ) || () () |   ) (      ) (   | (    \/                  
+| (__/ / | (____)|| |   | || || || |   | |      | |   | (__                      
+|  __ (  |     __)| |   | || |(_)| |   | |      | |   |  __)                     
+| (  \ \ | (\ (   | |   | || |   | |   | |      | |   | (                        
+| )___) )| ) \ \__| (___) || )   ( |___) (___   | |   | (____/\                  
+|/ \___/ |/   \__/(_______)|/     \|\_______/   )_(   (_______/                  
+ _______          _________ _______  _______  _        _______  _______  _______ 
+(  ____ \|\     /|\__   __/(  ____ \(       )( \      (  ____ \(  ____ \(  ____ \
+| (    \/( \   / )   ) (   | (    \/| () () || (      | (    \/| (    \/| (    \/
+| (_____  \ (_) /    | |   | (__    | || || || |      | (__    | (_____ | (_____ 
+(_____  )  \   /     | |   |  __)   | |(_)| || |      |  __)   (_____  )(_____  )
+      ) |   ) (      | |   | (      | |   | || |      | (            ) |      ) |
+/\____) |   | |      | |   | (____/\| )   ( || (____/\| (____/\/\____) |/\____) |
+\_______)   \_/      )_(   (_______/|/     \|(_______/(_______/\_______)\_______)
+          _______  ______           _________ _______                            
+|\     /|(  ____ \(  ___ \ |\     /|\__   __/(  ____ \|\     /|                  
+| )   ( || (    \/| (   ) )| )   ( |   ) (   | (    \/| )   ( |                  
+| | _ | || (__    | (__/ / | |   | |   | |   | (__    | | _ | |                  
+| |( )| ||  __)   |  __ (  ( (   ) )   | |   |  __)   | |( )| |                  
+| || || || (      | (  \ \  \ \_/ /    | |   | (      | || || |                  
+| () () || (____/\| )___) )  \   /  ___) (___| (____/\| () () |                  
+(_______)(_______/|/ \___/    \_/   \_______/(_______/(_______)       "
 ui_print "Enjoy a more private and faster webview, done systemlessly"
 ui_print "Don't forget my links:"
 ui_print "Social platforms:"
-ui_print " https://t.me/inlmagisk, https://t.me/bromitewebview, https://discord.gg/gTnDxQ6"
+ui_print " https://t.me/alexiadev, https://discord.gg/gTnDxQ6"
 ui_print "Donate at:"
 ui_print " https://paypal.me/linuxandria"
 ui_print " https://www.patreon.com/linuxandria_xda"
--- service.sh
+++ service.sh
@@ -1,25 +1,25 @@
 #!/sbin/.magisk/busybox/ash
+# shellcheck shell=dash
 SH=$(readlink -f "$0")
 MODDIR=$(dirname "$SH")
 exxit() {
 	  set +euxo pipefail
-	    [ $1 -ne 0 ] && abort "$2"
-	      exit $1
+	    [ "$1" -ne 0 ] && abort "$2"
+	      exit "$1"
       }
-exec 3>&2 2>$MODDIR/logs/service-verbose.log
+exec 3>&2 2>"$MODDIR"/logs/service-verbose.log
 set -x 2
 set -euo pipefail
 trap 'exxit $?' EXIT
 FINDLOG=$MODDIR/logs/find.log
 VERBOSELOG=$MODDIR/logs/service-verbose.log
-touch $VERBOSELOG
-touch $FINDLOG
+touch "$VERBOSELOG"
 echo "Started at $(date)"
-if [ -f $MODDIR/apk/webview.apk ] ;
+if test -f "$MODDIR"/apk/webview.apk ;
 then
 	sleep 20
-	pm install -r -g $MODDIR/apk/webview.apk 2>&3
-	rm -rf $MODDIR/apk/webview.apk
+	pm install -r -g "$MODDIR"/apk/webview.apk 2>&3
+	rm -rf "$MODDIR"/apk/webview.apk
 	echo "Installed bromite webview as user app.."
 	if pm list packages -a|grep -q com.google.android.webview 2>&3;
 	then
@@ -33,19 +33,9 @@ then
 else
 echo "File either moved or doesn't need installed...."
 fi
-while [ ! "$(getprop sys.boot_completed)" == "1" ] && [ -d /sdcard/Android ];
+while test ! "$(getprop sys.boot_completed)" = "1"  && test -d /sdcard/Android ;
 do sleep 0.5;
 done
-echo "SDCARD DIR contains:" > $FINDLOG
-find /sdcard/bromite >> $FINDLOG
-echo "Module DIR contains:" >> $FINDLOG
-find $MODDIR >> $FINDLOG
-cat $MODDIR/logs/props.log > $MODDIR/logs/verbose.log
-cat $MODDIR/logs/find.log >> $MODDIR/logs/verbose.log
-echo "\n\n" >> $MODDIR/logs/verbose.log
-cat $MODDIR/logs/postfsdata-verbose.log >> $MODDIR/logs/verbose.log
-echo "\n\n" >> $MODDIR/logs/verbose.log
-cat $MODDIR/logs/service-verbose.log >> $MODDIR/logs/verbose.log
-echo "\n\n\n" >> $MODDIR/logs/verbose.log
-cat $MODDIR/logs/aapt.log >> $MODDIR/logs/verbose.log
-cp -f $MODDIR/logs/* /sdcard/bromite/logs
+{ echo "SDCARD DIR contains:"; find /sdcard/bromite; echo "Module DIR contains:"; find "$MODDIR"; } > "$FINDLOG"
+tail -n +1 "$MODDIR"/logs/find.log cat "$MODDIR"/logs/props.log "$MODDIR"/logs/postfsdata-verbose.log "$MODDIR"/logs/service-verbose.log "$MODDIR"/logs/aapt.log > "$MODDIR"/logs/verbose.log 
+cp -rf "$MODDIR"/logs /sdcard/bromite/logs
