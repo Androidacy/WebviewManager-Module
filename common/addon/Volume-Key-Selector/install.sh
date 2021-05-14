4f176b3cd8d5942e477d0fdc06b13e71500cd54a
androidacy <admin@androidacy.com>
2021-05-13 22:15:22 -0400

    [Bugfix] Address volume key bug
    A lot more devices than expected have issues with keycheck. Let's use getevent where possible.
    
    Props to Zackptg5 for helping us with this issue.
    
    Signed-off-by: androidacy <admin@androidacy.com>

--- common/addon/Volume-Key-Selector/install.sh
+++ common/addon/Volume-Key-Selector/install.sh
@@ -1,31 +1,56 @@
-# shellcheck shell=ash
 # External Tools
+chmod -R 0755 $MODPATH/common/addon/Volume-Key-Selector/tools
 
-chmod -R 0755 "$MODPATH"/common/addon/Volume-Key-Selector/tools
-
-chooseport() {
+chooseport_legacy() {
   # Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
   # Calling it first time detects previous input. Calling it second time will do what we want
-  if [ -z "$1" ]; then
-    local delay=$1
-  else
-    local delay=3
-  fi
+  [ "$1" ] && local delay=$1 || local delay=3
   local error=false
   while true; do
-    timeout 0 "$MODPATH"/common/addon/Volume-Key-Selector/tools/"$ARCH32"/keycheck
-    timeout $delay "$MODPATH"/common/addon/Volume-Key-Selector/tools/"$ARCH32"/keycheck
-    local SEL=$?
-    if [ $SEL -eq 42 ]; then
+    timeout 0 $MODPATH/common/addon/Volume-Key-Selector/tools/$ARCH32/keycheck
+    timeout $delay $MODPATH/common/addon/Volume-Key-Selector/tools/$ARCH32/keycheck
+    local sel=$?
+    if [ $sel -eq 42 ]; then
       return 0
-    elif [ $SEL -eq 41 ]; then
+    elif [ $sel -eq 41 ]; then
       return 1
+    elif $error; then
+      abort "Volume key not detected!"
     else
-      $error && ui_print "⚠ Volume key error! Please edit config.txt and set FORCE_CONFIG to 1" && it_failed
       error=true
       echo "Volume key not detected. Try again"
     fi
   done
 }
+
+chooseport() {
+  # Original idea by chainfire and ianmacd @xda-developers
+  [ "$1" ] && local delay=$1 || local delay=3
+  local error=false 
+  while true; do
+    local count=0
+    while true; do
+      timeout $delay /system/bin/getevent -lqc 1 2>&1 > $TMPDIR/events &
+      sleep 0.5; count=$((count + 1))
+      if (`grep -q 'KEY_VOLUMEUP *DOWN' $TMPDIR/events`); then
+        return 0
+      elif (`grep -q 'KEY_VOLUMEDOWN *DOWN' $TMPDIR/events`); then
+        return 1
+      fi
+      [ $count -gt 6 ] && break
+    done
+    if $error; then
+      # abort "Volume key not detected!"
+      echo "Volume key not detected. Trying keycheck method"
+      export chooseport=chooseport_legacy VKSEL=chooseport_legacy
+      chooseport_legacy $delay
+      return $?
+    else
+      error=true
+      echo "Volume key not detected. Try again"
+    fi
+  done
+}
+
 # Keep old variable from previous versions of this
 VKSEL=chooseport
